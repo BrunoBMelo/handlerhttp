71d9a2b2f6ecb701f0121b6b8450109bb3836305
Bruno Melo <bruno.b.melo@live.com>
2022-04-11 19:03:10 -0300

    init

---
+++ porthttp.go
@@ -0,0 +1,53 @@
+package porthttp
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type MapRoute struct {
+	HttpMethod   string
+	RelativePath string
+	HandlerFunc  func(a interface{}) gin.HandlerFunc
+	IoC          func() interface{}
+}
+
+var r *gin.Engine
+
+func ConfigureMapRoute(maps func() []MapRoute) {
+	for _, mr := range maps() {
+		if mr.HttpMethod == http.MethodGet {
+			r.GET(mr.RelativePath, mr.HandlerFunc(mr.IoC()))
+		}
+
+		if mr.HttpMethod == http.MethodPost {
+			r.GET(mr.RelativePath, mr.HandlerFunc(mr.IoC()))
+		}
+
+		if mr.HttpMethod == http.MethodPut {
+			r.GET(mr.RelativePath, mr.HandlerFunc(mr.IoC()))
+		}
+
+		if mr.HttpMethod == http.MethodDelete {
+			r.GET(mr.RelativePath, mr.HandlerFunc(mr.IoC()))
+		}
+
+		if mr.HttpMethod == http.MethodHead {
+			r.GET(mr.RelativePath, mr.HandlerFunc(mr.IoC()))
+		}
+
+		if mr.HttpMethod == http.MethodOptions {
+			r.GET(mr.RelativePath, mr.HandlerFunc(mr.IoC()))
+		}
+
+		if mr.HttpMethod == http.MethodTrace {
+			r.GET(mr.RelativePath, mr.HandlerFunc(mr.IoC()))
+		}
+	}
+}
+
+func New() *gin.Engine {
+	r = gin.Default()
+	return r
+}
